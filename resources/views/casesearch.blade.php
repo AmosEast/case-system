7ab2763c6d59f13552f2fb01c42cfac75d43c8c8
lizhiqi <lizhiqi@didiglobal.com>
2021-03-01 20:15:54 +0800

    update

--- resources/views/casesearch.blade.php
+++ resources/views/casesearch.blade.php
@@ -12,33 +12,48 @@
                         <div id="left-navbar-main" class="sidebar-nav">
                             <form id="query-caselist-form" method="get" action="" class="form-inline">
                                 @csrf
-                                <ul class="nav nav-list collpse">
+                                <ul class="nav nav-list collapse">
                                     <li>
-                                        <a href="#select-books" class="nav-header" data-toggle="collapse" style="font-size: 1.3em;"><span class="glyphicon glyphicon-pencil"></span> &nbsp;&nbsp;&nbsp;案例模块</a>
-                                        <ul id="select-books" class="nav nav-list collapse" style="font-size: 1.1em;">
+                                        <a href="#select-mode" class="nav-header" data-toggle="collapse" style="font-size: 1.3em;">模式选择<span class="glyphicon glyphicon-collapse-down"></span></a>
+                                        <ul id="select-mode" class="nav nav-list collapse" style="font-size: 1.1em;">
                                             <div class="form-group">
-                                                <label for="case_id">案例编号</label>
-                                                <input type="text" class="form-control" id="case_id" name="case_id" placeholder="案例编号">
-                                            </div>
-                                            <span>&nbsp;&nbsp;</span>
-                                            <div class="form-group">
-                                                <label for="case_name">案例名称</label>
-                                                <input type="text" class="form-control" id="case_name" name="case_name" placeholder="案例名称">
-                                            </div>
-                                            <span>&nbsp;&nbsp;</span>
-                                            <div class="form-group">
-                                                <label for="case_province">案例省份</label>
-                                                <input type="text" class="form-control" id="case_province" name="case_province" placeholder="案例省份">
-                                            </div>
-                                            <span>&nbsp;&nbsp;</span>
-                                            <div class="form-group">
-                                                <label for="case_mode">案例模式</label>
-                                                <input type="text" class="form-control" id="case_mode" name="case_mode" placeholder="案例模式">
+                                                <label for="case_mode">请选择模式</label>
+                                                <select name="case_mode" id="case_mode" class="form-control">
+                                                    <option value="1">模式一</option>
+                                                    <option value="2">模式二</option>
+                                                    <option value="3">模式三</option>
+                                                    <option value="4">模式四</option>
+                                                </select>
                                             </div>
                                         </ul>
                                     </li>
                                     <li>
-                                        <a href="#query-block" class="nav-header" data-toggle="collapse" style="font-size: 1.3em;"><span class="glyphicon glyphicon-search"></span> &nbsp;&nbsp;&nbsp;检索模块</a>
+                                        <a href="#case-attribute" class="nav-header" data-toggle="collapse" style="font-size: 1.3em;">属性输入<span class="glyphicon glyphicon-collapse-down"></span></a>
+                                        <ul id="case-attribute" class="nav nav-list collapse" style="font-size: 1.1em;">
+                                            <div class="container">
+                                                <div class="raw">
+                                                    <div class="col-md-2">
+                                                        <strong>生成要素</strong>
+                                                    </div>
+                                                    <div class="col-md-10">
+                                                        <div class="form-group">
+                                                            <label for="case_name">案例名称</label>
+                                                            <input type="text" class="form-control" id="case_name" name="case_name" placeholder="案例名称">
+                                                        </div>
+                                                        <span>&nbsp;&nbsp;</span>
+                                                        <div class="form-group">
+                                                            <label for="case_province">案例省份</label>
+                                                            <input type="text" class="form-control" id="case_province" name="case_province" placeholder="案例省份">
+                                                        </div>
+                                                        <span>&nbsp;&nbsp;</span>
+                                                        <div class="form-group">
+                                                            <label for="case_mode">案例模式</label>
+                                                            <input type="text" class="form-control" id="case_mode" name="case_mode" placeholder="案例模式">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </ul>
                                     </li>
                                 </ul>
                                 <button type="submit" class="btn btn-primary">&nbsp;搜&nbsp;索&nbsp;</button>
